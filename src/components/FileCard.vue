b47300c55e7dc07c64d71840b81bae6b23930d15
xulihang <xulihanghai@163.com>
2023-09-01 14:50:28 +0800

    add a FileCard component to display the file info

---
+++ src/components/FileCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>{{ file.filename }}</ion-card-title>
+    </ion-card-header>
+    <ion-card-content>
+      <div class="item">
+        <ion-thumbnail slot="start" style="margin-right:10px;">
+          <img v-if="file.mimeType.indexOf('image') != -1" alt="image" :src="file.dataURL" />
+          <img v-else alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
+        </ion-thumbnail>
+        <div class="info">
+          <ion-label>{{ "Filesize: " + (file.filesize/1000).toFixed(2) + "KB" }}</ion-label>
+          <ion-label v-if="file.date">{{ "Date: " + file.date.toLocaleDateString() }}</ion-label>
+        </div>
+      </div>
+      
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script lang="ts" setup>
+export interface ScannedFile {
+  dataURL:string
+  filename:string
+  mimeType:string
+  filesize:number
+  date?:Date
+}
+const props = defineProps<{
+  file:ScannedFile
+}>()
+</script>
+
+<style scoped>
+.item {
+  display: flex;
+  align-items: start;
+}
+
+.info {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+
+.info ion-label {
+  margin-right: 10px;
+}
+</style>
