576be8bdaa4b803fc702d714b63f5f6423357ec9
xulihang <xulihanghai@163.com>
2023-09-01 17:31:19 +0800

    add a share button to the file card

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -11,7 +11,13 @@
         </ion-thumbnail>
         <div class="info">
           <ion-label>{{ "Filesize: " + (file.filesize/1000).toFixed(2) + "KB" }}</ion-label>
-          <ion-label>{{ "Date: " + file.date.toLocaleDateString() }}</ion-label>
+          <ion-label>{{ "Date: " + new Date(file.timestamp).toLocaleDateString() }}</ion-label>
+        </div>
+        <div class="span"></div>
+        <div class="action">
+          <ion-button @click="share" fill="clear">
+            <ion-icon slot="icon-only" :icon="shareOutline"/>
+          </ion-button>
         </div>
       </div>
     </ion-card-content>
@@ -20,10 +26,49 @@
 
 <script lang="ts" setup>
 import { ScannedFile } from '@/utils/FilesManager';
+import { IonButton,IonCard,IonCardContent,IonCardHeader,IonIcon } from '@ionic/vue';
+import { shareOutline } from 'ionicons/icons';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+import { Capacitor } from "@capacitor/core";
 
 const props = defineProps<{
   file:ScannedFile
 }>()
+
+const share = async () => {
+  if (Capacitor.isNativePlatform()) {
+    let writingResult = await Filesystem.writeFile({
+      path: props.file.filename,
+      data: props.file.dataURL,
+      directory: Directory.Cache
+    });
+    Share.share({
+      title: props.file.filename,
+      text: props.file.filename,
+      url: writingResult.uri,
+    });
+  }else{
+    const blob = dataURLtoBlob(props.file.dataURL);
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob);
+    link.download = props.file.filename;
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+}
+
+const dataURLtoBlob = (dataURL:string):Blob => {
+  const mime = dataURL.split(',')[0].split(':')[1].split(';')[0];
+  const binary = atob(dataURL.split(',')[1]);
+  const array = [];
+  for (let i = 0; i < binary.length; i++) {
+     array.push(binary.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {type: mime});
+}
+
 </script>
 
 <style scoped>
@@ -40,5 +85,8 @@ const props = defineProps<{
 
 .info ion-label {
   margin-right: 10px;
+}
+.span {
+  flex:1;
 }
 </style>
