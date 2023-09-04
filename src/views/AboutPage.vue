35fc564a0c3df2fd670b36d929dc916234ab55ef
xulihang <xulihanghai@163.com>
2023-09-04 17:05:17 +0800

    add link to the github repo

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -15,6 +15,7 @@
         <h2>QR Transfer</h2>
         <p>This is a QR code scanner which can be used to transfer data between devices via QR codes.</p>
         <p>Powered by Dynamsoft Barcode Reader.</p>
+        <p>Github repo: <a href="javascript:void()" @click="gitrepoClicked">https://github.com/tony-xlh/QRTransfer</a></p>
       </div>
     </ion-content>
   </ion-page>
@@ -22,6 +23,17 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
+import { Share } from '@capacitor/share';
+const gitrepoClicked = () => {
+  if (Capacitor.isNativePlatform()) {
+    Share.share({
+      text: "https://github.com/tony-xlh/QRTransfer",
+    });
+  }else{
+    window.open("https://github.com/tony-xlh/QRTransfer","_blank");
+  }
+}
 </script>
 <style scoped>
 .about {
